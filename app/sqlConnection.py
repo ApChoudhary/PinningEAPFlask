890e847ea795a15e4c873cf9fd9adf4380caba0d
ApChoudhary <55254460+apchoudhary@users.noreply.github.com>
2020-03-02 03:55:41 +0530

    APIs for getting data from database and inserting data into database created.

--- app/sqlConnection.py
+++ app/sqlConnection.py
@@ -1,12 +1,20 @@
 import pyodbc
 import json
+from configparser import ConfigParser
+
+
 
 def serverConnection():
+    parser = ConfigParser()
+    parser.read('config.ini')
+
+    username = parser.get('credentials', 'username')
+    password = parser.get('credentials', 'password')
     conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL SERVER};'
                           'Server=USHYDAPOOCH1\SQLEXPRESS;'
                           'Database=CDP_MONITOR;'
-                          'UID=apoochoudhary;'
-                          'PWD=Harkalpu!0511;'
+                          'UID='+username+';'
+                          'PWD='+password+';'
                           'Trusted_Connection=yes;'
                           )
 
@@ -28,6 +36,3 @@ def insertData(data):
     print('Insert into [Pinning Table] (ID, [Service Name], Hit, [Source], [Seq No]) Values ('+data["ID"]+', \''+data["Service_Name"]+'\', '+data["Hit"]+', \''+data["Source"]+'\', '+data["Seq_No"]+')')
     cursor.execute('Insert into [Pinning Table] (ID, [Service Name], Hit, [Source], [Seq No]) Values ('+data["ID"]+', \''+data["Service_Name"]+'\', '+data["Hit"]+', \''+data["Source"]+'\', '+data["Seq_No"]+')')
     conn.commit()
-    # print(data["ID"])
-# getSchema()
-# print(type(x))
